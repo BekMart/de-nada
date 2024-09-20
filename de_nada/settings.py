de1a1db227c02b10e74b55e38d1add8eb08a2dfc
Rebecca Martin <becki_213@hotmail.com>
2024-09-20 12:47:29 +0000

    generate secret key value and input in env.py and reference in settings.py. link to heroku

--- de_nada/settings.py
+++ de_nada/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zkj5lus(bhuh-zm5wt#64hvqfofgm#+dfeye$p6bg)v3*#y%gu'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
