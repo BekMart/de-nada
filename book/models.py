1044034999d4bb03d8aa79d88250ec36e87fb8dd
Rebecca Martin <becki_213@hotmail.com>
2024-09-23 06:00:33 +0000

    create models for restaurant and reservation to enable users to book a table

--- book/models.py
+++ book/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Pending"), (1, "Confirmed"), (2, "Cancelled"))
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='restaurant_images', null=True, blank=True)
+    tables = models.IntegerField(max_length=20)
+    capacity = models.IntegerField(default=80)
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    party_size = models.IntegerField()
+    reservation_date = models.DateField()
+    reservation_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
