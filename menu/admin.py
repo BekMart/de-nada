5195e8ef7354d1798ce664f1bcb0f6fc3b4f6744
Rebecca Martin <becki_213@hotmail.com>
2024-09-25 16:29:48 +0000

    create models for beverages and food within menu app. Register to admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Beverages, Food
 
 # Register your models here.
+admin.site.register(Beverages)
+admin.site.register(Food)
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Beverages(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    drink = models.CharField()
+    cost = models.FloatField()
+    calories = models.IntegerField()
+    vegetarian = models.BooleanField()
+    vegan = models.BooleanField()
+    gluten = models.BooleanField()
+
+class Food(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    food = models.CharField()
+    cost = models.FloatField()
+    calories = models.IntegerField()
+    vegetarian = models.BooleanField()
+    vegan = models.BooleanField()
+    gluten = models.BooleanField()
