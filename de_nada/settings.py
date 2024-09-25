b812c1ff3f7b31eebae13eaa4557b1f618dc4453
Rebecca Martin <becki_213@hotmail.com>
2024-09-25 15:53:00 +0000

    create basic style.css file for mobile device and ensure linked to all pages

--- de_nada/settings.py
+++ de_nada/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-bekmart-denada-8adq1lueftv.ws.codeinstitute-ide.net", ".herokuapp.com"]
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_summernote',
+    'bootstrap5',
     'book',
     'contact',
     'home',
--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 def homepage(request):
     print("About to render template")
