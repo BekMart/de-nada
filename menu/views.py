0575ee5cd8b9dd7f136d51101eb08f1dd182d4e3
Rebecca Martin <becki_213@hotmail.com>
2024-10-01 13:34:07 +0000

    display list of beverages with calories and cost on menu page

--- menu/views.py
+++ menu/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import Beverages, Food
 
 # Create your views here.
+# class BeveragesList(generic.ListView):
+#     queryset = Beverages.objects.all()
+#     template_name = "menu/menu.html"
+
 def displaymenu(request):
     print("About to render template")
+
+    beverages = Beverages.objects.all()
+    context = {'beverages': beverages}
 
     return render(
         request,
         "menu/menu.html",
+        context
     )
